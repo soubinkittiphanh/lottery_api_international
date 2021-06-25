230c87056a35fd951ec768cb846536b94fd475f8
soubin <kittiphanhsoubin@gmail.com>
2021-06-25 13:31:18 +0700

    LowHighLast

--- conn.js
+++ conn.js
@@ -3,16 +3,8 @@ const db = mysql.createPool({
   host: "mariadb-35079-0.cloudclusters.net",
    user: "root",
     password: "admin2020",
-    database: "lottery_savan",
+    database: "int_lottery_dev",
     port:35079,
-  // host: "localhost",
-  // user: "root",
-  // password: "",
-  // database: "lottery_pakse",
-  // port: 8080,
-  // waitForConnections: true,
-  // connectionLimit: 10,
-  // queueLimit: 0,
 });
 module.exports = {
   db,
--- connection.js
+++ connection.js
@@ -1,14 +1,9 @@
 const mysql = require("mysql");
 const db = mysql.createConnection({
-    // user: "root",
-    // host: "localhost",
-    // password: "",
-    // database: "lottery_pakse",
-    // port: 8080,
     user: "root",
     host: "mariadb-35079-0.cloudclusters.net",
     password: "admin2020",
-    database: "lottery_savan",
+    database: "int_lottery_dev",
     port:35079
   });
   
